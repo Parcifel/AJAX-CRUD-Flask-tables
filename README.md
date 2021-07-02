9055d3eacbbc6b60328a51566f4a010b266bc93a
Parcifel <pierrezaval09@gmail.com>
2021-07-02 10:48:27 +0200

    Finished

--- README.md
+++ README.md
@@ -5,6 +5,35 @@ This app is a prototype for loading database tables from mySQL , and
 displaying them in an web page through Flask and constructing the table with 
 Jinja.
 The web table has in-place-edits with AJAX to send the item to the Flask
-server for processing and updateing the mySQL database.
+server for processing and updating the mySQL database.
 
 ============================================================================
+
+in-place-edit:
+
+In place edit refer to editing some data in the place it is, without another
+page/ form being needed.
+In this example it has been done, so you can click on a cell of a table, and 
+edit the data of that cell, save it and it being saved to a database without 
+the page being reloaded.
+
+=============================================================================
+
+Files:
+
+.
+├── app
+│   ├── static
+│   │   └── edits.js
+│   │
+│   ├── templates
+│   │   └── table.html
+│   │
+│   ├── app.py
+│   ├── main.py
+│   └── db_config.py
+│ 
+├── README.md
+└── requirements.txt
+
+=============================================================================
--- app/main.py
+++ app/main.py
@@ -5,33 +5,41 @@ from app import app
 
 table_data = table
 
+# Default route
 @app.route('/', methods=['POST', 'GET'])
 def table():
     return render_template('table.html', table_data = table_data)
 
 
+# Save Data route to catch AJAX
 @app.route('/saveData', methods=['POST', 'GET'])
 def saveData():
+    # Only if request is POST
     if request.method == 'POST':
         isDataSame = False 
 
+        # Get variables send by AJAX
         data = request.form.get('data')
         idName = request.form.get('id')
 
+        # Devide element id into column name and row number
         for i in range( len(idName) ):
             try: 
                 int(idName[i])
                 break
             except:
-                nothing = None
+                nothing = None # Just something to fill the except condition, this is useless, but needed
 
+        # Create variables for column name and row number
         columnName = idName[:i]
         rowNum = int(idName[i:])
 
+        # Verify that value has changed, dont want to send unnecessary queries to database
         for row in table_data:
             if row[columnName] == data:
                 isDataSame = True
 
+        # Just some console feedbacks as no actual database is connected
         if isDataSame == False:
             print(data)
             print('columnt: ' + columnName)
@@ -44,13 +52,15 @@ def saveData():
             return('value did not change')
 
         return('dataSaved: ' + data)
+
+    # If request is not POST redirect to '/'
     return redirect('/')
 
 
 
 if __name__ == '__main__':
     app.run(
-        host='0.0.0.0',
+        host='127.0.0.1',
         port='1300',
         debug=True,
         threaded=True
